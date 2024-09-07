5ddf23bd9d150b972dca5f03d5ac587ed0140ecd
Keisuke Miura <toritoritori29@gmail.com>
2024-09-07 15:18:34 +0900

    feat: add color

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,28 @@
-import { test, expect } from "vitest";
+import { test, describe, expect } from "vitest";
+import { parseArgs } from "./index";
 
-test("add 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+describe("parseArgs", () => {
+	test("valid cases", () => {
+		expect(parseArgs(["a", "b", "--", "c", "d"])).toEqual({
+			args: ["a", "b"],
+			userCmd: [["c", "d"]],
+		});
+		expect(parseArgs(["a", "b", "--", "c", "d", "--", "e", "f"])).toEqual({
+			args: ["a", "b"],
+			userCmd: [
+				["c", "d"],
+				["e", "f"],
+			],
+		});
+	});
+
+	test("invalid cases", () => {
+		expect(() => parseArgs(["a", "b", "--", "c", "d", "--"])).toThrow(
+			"Invalid command: The command cannot be end with '--'.",
+		);
+		expect(() => parseArgs(["a", "b", "--", "--", "c", "d"])).toThrow(
+			"Invalid command: The command cannot be empty. Verify that '--' is not repeated consecutively.",
+		);
+		// expect(() => parseArgs(["--", "--", "c", "d"])).toThrow("Invalid command: The command cannot be empty. Verify that '--' is not repeated consecutively.");
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
-import { spawn } from "node:child_process";
+import { type ChildProcess, spawn } from "node:child_process";
+import chalk from "chalk";
+
+import { fileURLToPath } from "node:url";
+
+const COLOR_ARRAY: CallableFunction[] = [
+	chalk.red,
+	chalk.green,
+	chalk.yellow,
+	chalk.blue,
+	chalk.magenta,
+	chalk.cyan,
+];
 
 interface Args {
 	args: string[];
 	userCmd: string[][];
 }
 
-function parseArgs(argv: string[]): Args {
-	return {
-		args: [],
-		userCmd: [],
-	};
-}
-
-function writeLog(tag: string, msg: string) {
-	const line = `[${tag}] ${msg}`;
-	process.stdout.write(line);
-}
-
-function main(argv: string[]) {
+export function parseArgs(argv: string[]): Args {
 	const commands: string[][] = [];
 	let current: string[] = [];
+
+	if (argv.at(-1) === "--") {
+		throw new Error("Invalid command: The command cannot be end with '--'.");
+	}
 
 	for (const arg of argv) {
 		if (arg === "--") {
@@ -31,17 +35,98 @@ function main(argv: string[]) {
 	}
 	commands.push(current);
 
-	const userCmd = commands.slice(1);
-	for (let i = 0; i < userCmd.length; i++) {
-		const cmd = userCmd[i];
-		const process = spawn(cmd[0], cmd.slice(1));
+	// Check if there is an empty command
+	if (commands.slice(1).some((cmd) => cmd.length === 0)) {
+		throw new Error(
+			"Invalid command: The command cannot be empty. Verify that '--' is not repeated consecutively.",
+		);
+	}
 
-		const tag = (i + 1).toString();
+	return {
+		args: commands[0],
+		userCmd: commands.slice(1),
+	};
+}
+
+function writeSystemLog(msg: string) {
+	const line = `[SYSTEM] ${msg}\n`;
+	process.stdout.write(chalk.gray(line));
+}
+
+class Process {
+	id: number;
+	tag: string;
+	cmd: string[];
+	process: ChildProcess | undefined;
+
+	constructor(id: number, tag: string, cmd: string[]) {
+		this.id = id;
+		this.tag = tag;
+		this.cmd = cmd;
+		this.process = undefined;
+	}
+
+	start() {
+		const process = spawn(this.cmd[0], this.cmd.slice(1));
 		process.stdout.on("data", (data) => {
-			writeLog(tag, data);
+			this.writeLog(data.toString());
 		});
+
+		process.stdout.on("close", () => {
+			writeSystemLog(`Process ${this.tag} is exited`);
+			this.process = undefined;
+		});
+		this.process = process;
+	}
+
+	kill() {
+		if (this.process === undefined) {
+			return;
+		}
+		this.process.kill();
+		writeSystemLog(`Sending SIGTERM signal for the process ${this.tag}`);
+	}
+
+	writeLog(msg: string) {
+		const lines = msg.split(/\n/);
+		const colorFunc = COLOR_ARRAY[this.id % COLOR_ARRAY.length];
+		for (const line of lines) {
+			const text = `[${this.tag}] ${line}\n`;
+			process.stdout.write(colorFunc(text));
+		}
 	}
 }
 
-const arg = process.argv.slice(2);
-main(arg);
+function main(argv: string[]) {
+	const arg = parseArgs(argv);
+
+	const processList: Process[] = [];
+	for (let i = 0; i < arg.userCmd.length; i++) {
+		const cmd = arg.userCmd[i];
+		const tag = (i + 1).toString();
+		const process = new Process(i, tag, cmd);
+		process.start();
+		processList.push(process);
+	}
+
+	// handle SIGINT = CTRL+C
+	process.once("SIGINT", () => {
+		writeSystemLog("Received SIGINT signal");
+		for (const process of processList) {
+			process.kill();
+		}
+	});
+
+	// handle EOF = CTRL+D
+	process.stdin.resume();
+	process.stdin.on("end", () => {
+		writeSystemLog("Received EOF signal");
+		for (const process of processList) {
+			process.kill();
+		}
+	});
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+	main(process.argv.slice(2));
+}
