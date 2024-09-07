8f382f892160538cec0c03ff8c601a813419df40
Keisuke Miura <toritoritori29@gmail.com>
2024-09-07 18:14:22 +0900

    feat: chore

---
+++ src/cli.test.ts
@@ -0,0 +1,28 @@
+import { test, describe, expect } from "vitest";
+import { parseArgs } from "./cli";
+
+describe("parseArgs", () => {
+	test("valid cases", () => {
+		expect(parseArgs(["a", "b", "--", "c", "d"])).toEqual({
+			args: ["a", "b"],
+			userCmd: [["c", "d"]],
+		});
+		expect(parseArgs(["a", "b", "--", "c", "d", "--", "e", "f"])).toEqual({
+			args: ["a", "b"],
+			userCmd: [
+				["c", "d"],
+				["e", "f"],
+			],
+		});
+	});
+
+	test("invalid cases", () => {
+		expect(() => parseArgs(["a", "b", "--", "c", "d", "--"])).toThrow(
+			"Invalid command: The command cannot be end with '--'.",
+		);
+		expect(() => parseArgs(["a", "b", "--", "--", "c", "d"])).toThrow(
+			"Invalid command: The command cannot be empty. Verify that '--' is not repeated consecutively.",
+		);
+		// expect(() => parseArgs(["--", "--", "c", "d"])).toThrow("Invalid command: The command cannot be empty. Verify that '--' is not repeated consecutively.");
+	});
+});
